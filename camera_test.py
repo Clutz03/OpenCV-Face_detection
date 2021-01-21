f41c4c5f2bba1c84e5b14a7917a9d70abd576885
Desmond Phee <nocolour@gmail.com>
2021-01-21 11:28:56 +0800

    Fix the bug press "q" exit

--- camera_test.py
+++ camera_test.py
@@ -1,7 +1,8 @@
-import numpy as np
 import cv2
+
 cap = cv2.VideoCapture(0)
-while(True):
+
+while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1) # Flip camera vertically "-1"
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -9,5 +10,7 @@ while(True):
     cv2.imshow('frame', frame)
     cv2.imshow('gray', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        breakcap.release()
+        break
+
+cap.release()
 cv2.destroyAllWindows()
